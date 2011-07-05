631cbc26a7fd20ce2fc1b875bb2387491fd5a35e
Paraskevi Papa <pamagi_123@yahoo.gr>
2011-07-05 14:50:15 +0300

    changes05072011

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,11 +2,11 @@ class Article < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   has_many :comments
-  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :picture, :styles => { :medium => "200x200>", :thumb => "100x100>" }
   default_scope order("created_at DESC")
 
   validates :title, :presence => true, :uniqueness => true, :length => { :minimum => 3, :maximum => 40 }
-  validates :body, :presence => true, :uniqueness => true, :length => { :minimum => 3, :maximum => 500 }
+  validates :body, :presence => true, :uniqueness => true, :length => { :minimum => 3, :maximum => 1000 }
   validates :category, :presence => true
   validates :user, :presence => true
 
