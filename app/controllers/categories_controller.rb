a6de6f49268352010d45a08848cd6c2d30bfcc3d
Paraskevi Papa <pamagi_123@yahoo.gr>
2011-09-07 20:07:08 +0300

    7092011_1

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.xml
+  before_filter :authenticate_user!
   def index
     @categories = Category.all
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,12 @@ class Category < ActiveRecord::Base
   has_many :articles
 
   validates :name, :presence => true
+
+  def editable_by?(user)
+   user.present? and and user.admin?
+  end
+
+  def deletable_by?(user)
+   user.present? and user.admin?
+  end
 end
